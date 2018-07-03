fd7cd024fbb522ec2159bbe0640507ea3afba85a
Garrett S <garrettsherm@gmail.com>
2018-07-03 17:50:06 -0400

    finished lesson 4

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 const Single = React.createClass({
-	render(){
-		return(
-			<div className="single-photo">
-			Single
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="single-photo">
+      I'm the single
+      </div>
+    )
+  }
 });
 
 export default Single;
