2471a4b047ca7421e6da40a71d2d8d091484952d
Garrett S <garrettsherm@gmail.com>
2018-07-04 12:36:27 -0400

    finished lesson 7

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,23 @@
+export function increment(index) {
+	return {
+		type: 'INCREMENT_LIKES',
+		index
+	};
+};
+
+export function addComment(postID, author, comment){
+	return {
+		type: 'ADD_COMMENT',
+		postId,
+		author,
+		comment
+	};
+};
+
+export function removeComent(postId, i){
+	return {
+		type: 'REMOVE_COMMENT',
+		i,
+		postId
+	};
+};
